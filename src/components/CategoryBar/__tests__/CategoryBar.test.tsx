57fc3f6f36bf4e7c542bb49983fd4a9314a58dc9
Rafael Martins <41962783+rmzaoo@users.noreply.github.com>
2022-06-14 17:01:56 +0100

    tests

---
+++ src/components/CategoryBar/__tests__/CategoryBar.test.tsx
@@ -0,0 +1,38 @@
+import { describe, test, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import CategoryBar from "../CategoryBar";
+import { BrowserRouter } from "react-router-dom";
+import * as api from "../../../api/productsFetch";
+
+describe("CategoryBar", () => {
+  test("renders without crashing", () => {
+    const result = render(
+      <BrowserRouter>
+        <CategoryBar />
+      </BrowserRouter>
+    );
+    expect(result).toMatchSnapshot();
+  });
+
+  test("renders categories", () => {
+    const categories = [
+      "category1",
+      "category2",
+      "category3",
+      "category4",
+      "category5",
+      "category6",
+    ];
+    vi.spyOn(api, "fetchCategories").mockReturnValue(categories);
+
+    render(
+      <BrowserRouter>
+        <CategoryBar />
+      </BrowserRouter>
+    );
+
+    categories.forEach((category) => {
+      expect(screen.getByText(category));
+    });
+  });
+});
