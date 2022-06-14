acb4c63bce78ad1fcb1871d4c39a36119ca51b5c
Rafael Martins <41962783+rmzaoo@users.noreply.github.com>
2022-06-14 11:16:43 +0100

    title recent prodcuts homepage

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -13,6 +13,7 @@ import {
   ContainerDealitDetails,
   DetailsInfo,
   FaqContainer,
+  RecentProductsContainer,
 } from "./style";
 import detailsSVG from "../../assets/details.svg";
 import ProductListing from "../../components/ProductListing/ProductListing";
@@ -97,7 +98,10 @@ const Homepage = () => {
           backgroundColor: "#141414",
         }}
       >
-        <ProductListing oneColumn={true} />
+        <RecentProductsContainer>
+          <h2>Recent Products</h2>
+          <ProductListing oneColumn={true} />
+        </RecentProductsContainer>
       </ParallaxLayer>
       <ParallaxLayer
         speed={0.5}
