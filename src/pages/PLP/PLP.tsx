9aa5f27a7344595067d03b857a203f7f0ef06b8c
André Parra <andre.filipe.parra@gmail.com>
2022-06-17 13:05:49 +0100

    fixing

--- src/pages/PLP/PLP.tsx
+++ src/pages/PLP/PLP.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import {
   Category,
   Container,
@@ -7,31 +7,16 @@ import {
   SafeContainer,
   StyledProductListing,
 } from "./style";
-import { queryParam } from "../../utils/queryParams";
-import { PrimaryButton } from "../../components/PrimaryButton/style";
-import { fetchProductByCategoryPerPage } from "../../api/productsFetch";
+import { IParallax, Parallax, ParallaxLayer } from "@react-spring/parallax";
+import { useParams } from "react-router-dom";
+import { ProductProps } from "../../components/Product/Product";
+import ProductListing from "../../components/ProductListing/ProductListing";
+import { useProductByCategoryFetcher } from "../../hooks/products/useProductByCategoryFetcher";
 
 const PLP = () => {
-  const pageQueryParam = queryParam("page") || "1";
-  const categoryQueryParam = queryParam("category") || "recent";
-
-  const [currentCategory, setCurrentCategory] = useState(categoryQueryParam);
-  const [currentPage, setCurrentPage] = useState(pageQueryParam);
-  const [products, setProducts] = useState([]);
-
-  useLayoutEffect(() => {
-    setCurrentCategory(categoryQueryParam);
-    setCurrentPage(pageQueryParam);
-
-    fetchProductByCategoryPerPage(
-      parseInt(pageQueryParam),
-      categoryQueryParam
-    ).then((data) => {
-      setProducts(data);
-    });
-  }, [categoryQueryParam, pageQueryParam]);
-
-  /*const { category1, category2 } = useParams();
+  const parallax = useRef<IParallax>(null!);
+  const [currentPage, setCurrentPage] = useState(0);
+  const { category1, category2 } = useParams();
   let mainProductsCategory: ProductProps[] = [];
 
   if (category2) {
@@ -83,35 +68,65 @@ const PLP = () => {
             <MainText>Scroll down to see all the products!</MainText>
           </CategoryDiv>
           <ProductListing oneColumn={false} products={mainProductsCategory} />
-        </ParallaxLayer>*/
+        </ParallaxLayer>
 
+        <LayerDividing
+          offset={0.95}
+          speed={0.8}
+          style={{
+            backgroundColor: "white",
+          }}
+        >
+          <LayerDividing
+            offset={0.95}
+            speed={1}
+            style={{
+              clipPath: "polygon(100% 20%, 100% 56%, 0 100%, 0 65%)",
+              backgroundColor: "rgb(0, 180, 204)",
+            }}
+          />
+        </LayerDividing>
 
-  return (
-    <Container>
-      <SafeContainer>
-        <Category>
-          <h1>Category</h1>
-          <span>{currentCategory}</span>
-        </Category>
-
-        <ContainerProducts>
-          <StyledProductListing oneColumn={false} products={products} />
-        </ContainerProducts>
-        <PageBottoms>
-          <PrimaryButton
-            onClick={() => setCurrentPage(`${parseInt(currentPage) - 1}`)}
-          >
-            Previous
-          </PrimaryButton>
-          <span>Page {currentPage}</span>
-          <PrimaryButton
-            onClick={() => setCurrentPage(`${parseInt(currentPage) + 1}`)}
-          >
-            Next
-          </PrimaryButton>
-        </PageBottoms>
-      </SafeContainer>
-    </Container>
+        <ParallaxLayer
+          offset={1.12}
+          speed={0.5}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}
+        >
+          <ProductListing oneColumn={false} products={mainProductsCategory} />
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={1.9}
+          speed={0.8}
+          style={{
+            backgroundColor: "white",
+            clipPath: "polygon(100% 14%, 100% 56%, 0 100%, 0 66%)",
+          }}
+        >
+          <LayerDividing
+            offset={1}
+            speed={1.5}
+            style={{
+              clipPath: "polygon(100% 20%, 100% 56%, 0 100%, 0 65%)",
+              backgroundColor: "rgb(0, 180, 204)",
+            }}
+          />
+        </ParallaxLayer>
+      </Parallax>
+      <ScrollTo onClick={() => scroll(0)}>
+        <p>Page 1</p>
+      </ScrollTo>
+      <ScrollTo onClick={() => scroll(1)}>
+        <p>Page 2</p>
+      </ScrollTo>
+      <ScrollTo onClick={() => scroll(2)}>
+        <p>Page 3</p>
+      </ScrollTo>
+    </MainContainer>
   );
 };
 export default PLP;
