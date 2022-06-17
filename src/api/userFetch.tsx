9190ecd421360e8e9bb8ef5fe6fe62e77caf3b92
rmzaoo <mazer.yt.email@gmail.com>
2022-06-17 09:46:22 +0100

    refactor: changed random products endpoint

--- src/api/userFetch.tsx
+++ src/api/userFetch.tsx
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const baseURL = "http://10.10.225.129:3220/dealit/api";
-const localUrl = "http://localhost:3220/dealit/api";
+const baseUrl = "http://localhost:3220/dealit/api";
 
 export const fetchUserById = async (id: number) => {
-  return await axios.get(`${localUrl}/users/${id}`).then((response: any) => {
+  return await axios.get(`${baseUrl}/users/${id}`).then((response: any) => {
     return response.data;
   });
 };
