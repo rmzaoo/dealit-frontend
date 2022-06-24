6660c503e8c72da8574173f252abde16dd296139
GenesisMRL <nunomiglima@gmail.com>
2022-06-24 11:48:37 +0100

    removed unnecessary useEffect in Categories bar, added 2nd category to PLP breadcrumb and made 1st cat clickable, fixed categories filter in PLP

--- src/components/CategoryBar/CategoryBar.tsx
+++ src/components/CategoryBar/CategoryBar.tsx
@@ -3,39 +3,29 @@ import { useNavigate, useParams } from "react-router-dom";
 import TextLink from "../TextLink/TextLink";
 import { Container, SubsContainer } from "./style";
 import { useSelector } from "react-redux";
+import PLP from "../../pages/PLP/PLP";
 
 interface Props {
   className?: string;
 }
 
 const CategoryBar = ({ className }: Props) => {
-  const { category1 } = useParams();
+  const params = useParams();
   const categories = useSelector((state: any) => state.categories);
   const navigate = useNavigate();
-  const [subCats, setSubCats] = useState<string[]>();
 
-  function navigateMainCat(category: string) {
+  function navigateMainCat(this: any, category: string) {
     navigate(`/products/${category}`);
     window.location.reload();
   }
   function navigateSubCat(category: string) {
-    navigate(`/products/${category1}/${category}`);
+    navigate(`/products/${params.category1}/${category}`);
     window.location.reload();
   }
-
-  useEffect(() => {
-    categories.forEach((main: any) => {
-      if (main.name === category1) {
-        const subs = main.subCategories.map((sub: any) => sub.name);
-        setSubCats(subs);
-      }
-    });
-  }, [category1]);
 
   return !categories ? null : (
     <>
       <Container className={className}>
-        {console.log(subCats)}
         {categories.map(
           (category: { mainCategory: string; subCategories: string[] }) => (
             <TextLink
@@ -47,13 +37,15 @@ const CategoryBar = ({ className }: Props) => {
           )
         )}
       </Container>
-      {category1 && (
+      {params.category1 && (
         <SubsContainer className={className}>
-          {categories.filter((cat: any) => cat.mainCategory === category1)[0].subCategories.map((category: string) => (
-            <TextLink onClick={() => navigateSubCat(category)} key={category}>
-              {category}
-            </TextLink>
-          ))}
+          {categories
+            .filter((cat: any) => cat.mainCategory === params.category1)[0]
+            .subCategories.map((category: string) => (
+              <TextLink onClick={() => navigateSubCat(category)} key={category}>
+                {category}
+              </TextLink>
+            ))}
         </SubsContainer>
       )}
     </>
