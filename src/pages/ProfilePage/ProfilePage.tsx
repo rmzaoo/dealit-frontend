e0d2b7ce15b9dd75b90141cefbc3fbb602b8ee1c
Rafael Martins <41962783+rmzaoo@users.noreply.github.com>
2022-06-30 14:31:38 +0100

    profile page

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,14 +1,28 @@
 import React from "react";
-import { ProfilePageBody, ProfilePageTitle } from "./style";
+import {
+  BannerContainer,
+  BannerProfile,
+  ContainerPhotos,
+  PhotoContainer,
+  PhotoProfile,
+  ProfileInfo,
+  ProfilePageBody,
+} from "./style";
 
-const ProfilePage = () => {
-  return (
-    <ProfilePageBody>
-      <ProfilePageTitle>
-        <h2>My Profile</h2>
-      </ProfilePageTitle>
-    </ProfilePageBody>
-  );
-};
+const ProfilePage = () => (
+  <ProfilePageBody>
+    <ContainerPhotos>
+      <BannerContainer>
+        <BannerProfile src="https://pbs.twimg.com/profile_banners/44196397/1576183471/1500x500" />
+      </BannerContainer>
+      <PhotoContainer>
+          <PhotoProfile src="https://pbs.twimg.com/profile_images/1529956155937759233/Nyn1HZWF_400x400.jpg" />
+          <ProfileInfo>
+            
+          </ProfileInfo>
+      </PhotoContainer>
+    </ContainerPhotos>
+  </ProfilePageBody>
+);
 
 export default ProfilePage;
--- src/pages/ProfilePage/style.ts
+++ src/pages/ProfilePage/style.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import DashboardPageTitle from "../../components/DashboardPageTitle/DashboardPageTitle";
 
-export const ProfilePageBody = styled.div`
+const ProfilePageBody = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: center;
@@ -10,10 +10,50 @@ export const ProfilePageBody = styled.div`
   height: 100%;
 `;
 
-export const ProfilePageTitle = styled(DashboardPageTitle)`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  width: 80%;
-  height: 20%;
+const ContainerPhotos = styled.div`
+  width: 100%;
+  height: 450px;
 `;
+
+const BannerContainer = styled.div`
+  height: 300px;
+`;
+
+const BannerProfile = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-bottom: 1px solid #169;
+`;
+
+const PhotoContainer = styled.div`
+  height: 150px;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+const PhotoProfile = styled.img`
+  height: 300px;
+  width: auto;
+  object-fit: cover;
+  border-radius: 50%;
+  border: 1px solid #169;
+  margin-top: -150px;
+  margin-left: 50px;
+`;
+
+const ProfileInfo = styled.div`
+
+
+`;
+
+export {
+  ProfilePageBody,
+  ContainerPhotos,
+  BannerProfile,
+  PhotoProfile,
+  BannerContainer,
+  PhotoContainer,
+  ProfileInfo
+};
